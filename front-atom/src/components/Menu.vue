b977c97ee9451ea1f3395fa4bf99f7f22a55a97d
GaoYang <111@111.com>
2020-03-23 10:32:39 +0800

    前端框架设计

---
+++ front-atom/src/components/Menu.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <el-container style="height: 100%; border: 1px">
+      <el-header>
+      </el-header>
+      <el-aside style="width: 15%;">
+        <el-menu>
+          <el-submenu index="1">
+            <template slot="title"><i class="el-icon-message"></i>个人信息</template>
+            <el-menu-item-group>
+              <el-menu-item index="1-1">登录维护</el-menu-item>
+              <el-menu-item index="1-2">信息维护</el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+          <el-submenu index="2">
+            <template slot="title"><i class="el-icon-message"></i>编辑模块</template>
+          </el-submenu>
+          <el-submenu index="3">
+            <template slot="title"><i class="el-icon-message"></i>开源模块</template>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+    </el-container>
+  </div>
+</template>
+
+<style>
+  .el-header {
+    background-color: #B3C0D1;
+    color: #333;
+    line-height: 60px;
+  }
+
+  .el-aside {
+    color: #333;
+  }
+</style>
+
+<script>
+</script>
--- front-atom/src/router/index.js
+++ front-atom/src/router/index.js
@@ -1,21 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-// import HelloWorld from '@/components/HelloWorld'
-import Home from '@/components/Home'
+import HelloWorld from '@/components/HelloWorld'
+import Menu from '@/components/Menu'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'HelloWorld',
-    //   component: HelloWorld
-    // },
     {
-      path: '/home',
-      name: 'Home',
-      component: Home
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/menu',
+      name: 'Menu',
+      component: Menu
     }
   ]
 })
